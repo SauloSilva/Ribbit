04edb6b6790a9aca88c5043b6b055cb01df076d6
saulo da silva santiago <saulodasilvasantiago@gmail.com>
2014-04-17 02:40:05 -0300

    fazendo a escolha de imagens e videos

--- src/com/saulo/ribbit/MainActivity.java
+++ src/com/saulo/ribbit/MainActivity.java
@@ -1,10 +1,12 @@
 package com.saulo.ribbit;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -22,7 +24,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.widget.Toast;
-
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
 
@@ -38,6 +39,8 @@ public class MainActivity extends ActionBarActivity implements
 	
 	public static final int MEDIA_TYPE_IMAGE = 4;
 	public static final int MEDIA_TYPE_VIDEO = 5;
+	
+	public static final int FILE_SIZE_LIMIT = 1024*1024*10;
 	
 	protected Uri mMediaUri;
 	
@@ -69,8 +72,15 @@ public class MainActivity extends ActionBarActivity implements
 					}
 					break;
 				case 2:
+					Intent choosePhotoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+					choosePhotoIntent.setType("image/*");
+					startActivityForResult(choosePhotoIntent, PICK_PHOTO_REQUEST);
 					break;
 				case 3:
+					Intent chooseVideoIntent = new Intent(Intent.ACTION_GET_CONTENT);
+					chooseVideoIntent.setType("video/*");
+					Toast.makeText(MainActivity.this, R.string.vide_file_size_warning, Toast.LENGTH_LONG).show();
+					startActivityForResult(chooseVideoIntent, PICK_VIDEO_REQUEST);
 					break;
 			}
 		}
@@ -170,9 +180,48 @@ public class MainActivity extends ActionBarActivity implements
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == RESULT_OK) { 
-			Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-			mediaScanIntent.setData(mMediaUri);
-			sendBroadcast(mediaScanIntent);
+		
+			if (requestCode == PICK_PHOTO_REQUEST || requestCode == PICK_VIDEO_REQUEST) {
+			
+				if (data == null ) {
+					Toast.makeText(this, getString(R.string.general_error), Toast.LENGTH_LONG).show();
+				} else {
+					mMediaUri = data.getData();
+				}
+				
+				if (requestCode == PICK_VIDEO_REQUEST) {
+					int fileSize = 0;
+					InputStream inputStream = null;
+					
+					try {
+						inputStream = getContentResolver().openInputStream(mMediaUri);
+						fileSize = inputStream.available();						
+					} catch (FileNotFoundException e) {
+						Log.e(TAG, "Exception caught: " + e.getMessage());
+						Toast.makeText(this, R.string.error_openning_file, Toast.LENGTH_LONG).show();
+						return;
+					} catch (IOException e) {
+						Log.e(TAG, "Exception caught: " + e.getMessage());
+						Toast.makeText(this, R.string.error_openning_file, Toast.LENGTH_LONG).show();
+						return;
+					} finally { 
+						try {
+							inputStream.close();
+						} catch (IOException e) { /* Intentitional blank */ }
+					}
+					
+					if (fileSize >= FILE_SIZE_LIMIT) {
+						Toast.makeText(this, R.string.error_file_size_to_large, Toast.LENGTH_SHORT).show();
+						return;
+					}
+				}
+				
+			} else {
+				Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+				mediaScanIntent.setData(mMediaUri);
+				sendBroadcast(mediaScanIntent);
+			}
+		
 		} else if (resultCode != RESULT_CANCELED) {
 			Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG).show();
 		}
