041b3674bceb68df2a83208922548ceef93c3c02
saulo da silva santiago <saulodasilvasantiago@gmail.com>
2014-04-19 13:53:54 -0300

    eventos para visualizar imagem e videos enviados.

---
+++ src/com/saulo/ribbit/ViewImageActivity.java
@@ -0,0 +1,24 @@
+package com.saulo.ribbit;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class ViewImageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_image);
+		
+		ImageView imageView = (ImageView) findViewById(R.id.imageView);
+		Uri imageUri = getIntent().getData();
+		String senderName = getIntent().getExtras().getString(ParseConstants.KEY_SENDER_NAME);
+		
+		setTitle(getString(R.string.image_sender_to) + " " + senderName);
+		Picasso.with(this).load(imageUri.toString()).into(imageView);
+	}
+}
